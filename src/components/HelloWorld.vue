063e43469d00b874ce0cf2ef2be8336f5ce6f6bd
刘晨阳 <chenyang.liu@vigekoo.com>
2021-08-11 12:22:42 +0800

    lcy2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="hello">
 		<h1>{{ msg }}</h1>
-		<p>123</p>
+		<p>123456</p>
 		<p>
 			For a guide and recipes on how to configure / customize this project,<br />
 			check out the
